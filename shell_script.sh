f11bd03779478c53c22dd2d08427b76b9a623219
Manu S Bhat <cs15lwi23axm@ieng6-201.ucsd.edu>
2023-02-23 08:39:10 -0800

    Add shellscript

---
+++ shell_script.sh
@@ -0,0 +1,2 @@
+git clone git@github.com:mbhat-ucsd/lab7.git ; cd lab7 ; javac -cp ".:./lib/*" *.java ; java -cp ".:./lib/*" org.junit.runner.JUnitCore TestListExamples ; vim ListExamples.j    ava ; javac -cp ".:./lib/*" *.java ; java -cp ".:./lib/*" org.junit.runner.JUnitCore TestListExamples ;  git add -A ; git commit -m "X" ; git push
+
